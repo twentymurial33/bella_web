292f5705b1d97307f422d8b16ff4b3fbcfcf049a
Murial Anindo <murial.anindo@lonett.com>
2025-03-04 08:37:30 -0500

    1st commit for project

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
